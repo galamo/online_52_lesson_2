6bb3688fe377f8d5ac64008c3eb3d9a817e5262f
Gal Amouyal <gal.amouyal@ibm.com>
2020-04-21 18:32:07 +0300

    board model is created and generat sticky notes

--- models/sticky.js
+++ models/sticky.js
@@ -3,3 +3,15 @@ function Sticky(id, desc) {
   this.desc = desc;
   this.createdAt = new Date();
 }
+
+function Board(stickyNotes = []) {
+  this.stickyNotes = stickyNotes || generateStickyNotes();
+}
+
+function generateStickyNotes() {
+  const stickyNotes = [];
+  for (let index = 0; index < 100; index++) {
+    stickyNotes.push(new Sticky(null, `description_${index}`));
+  }
+  return stickyNotes;
+}
